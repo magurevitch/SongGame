2cede6f318198d80c105a8644add6ae882122c0f
Matthew Gurevitch <matthewgurevitch@gmail.com>
2024-04-17 12:50:06 -0400

    refactor: making cli run_command a more testable function

--- cli.py
+++ cli.py
@@ -1,73 +1,73 @@
 from src.SongBank import SongBank
 from src.DataStore import DataStore
 
-def format_title(raw_title) -> str:
+def format_title(raw_title: str) -> str:
     song_name, artist = raw_title.split("-", 1)
     return "{} - {}".format(song_name.strip(), artist.strip())
 
-def find_arguments(raw_input):
+def find_arguments(raw_input:str) -> list[str, None]:
     command, *arguments = raw_input.split(" ", 1)
     return command.strip(), None if arguments == [] else arguments[0]
 
-def get_song_youtube_link(song_title):
+def get_song_youtube_link(song_title: str) -> str:
     youtube_format = "https://www.youtube.com/results?search_query="
     cleaned_song_title = song_title.replace(" ", "+").replace("'", "%27")
     return youtube_format + cleaned_song_title
 
-def run_cli():
-    data_store = DataStore()
+def run_command(command: str, arguments: str | None) -> str:
     song_bank = SongBank()
+    data_store = DataStore()
+    match command:
+        case "reset":
+            data_store.reset_tables()
+            return "all data wiped"
+        case "songs":
+            songs = list(data_store.get_songs())
+            return "\n".join(str(i) + " - " + songs[i] + " (" + get_song_youtube_link(songs[i]) + ")" for i in range(len(songs)))
+        case "players":
+            players = data_store.get_all_players()
+            return ", ".join(players)
+        case "votes":
+            songs = list(data_store.get_songs())
+            return "\n".join(song + ": " + str(data_store.get_votes(song)) for song in songs)
+        case "list":
+            player, songs = arguments.split(" ", 1)
+            song_bank.add_player(player, [format_title(title) for title in songs.split(",")])
+            return "added songs for player " + player
+        case "merge":
+            index1, index2 = arguments.split()
+            songs = list(data_store.get_songs())
+            song_bank.merge_songs(songs[int(index1)], songs[int(index2)])
+            return "merged song {} into song {}".format(songs[int(index1)], songs[int(index2)])
+        case "vote":
+            votes = set(arguments.split())
+            songs = list(data_store.get_songs())
+            if len(votes) > len(songs):
+                raise IndexError()
+            song_bank.add_votes([songs[int(i)] for i in votes])
+            return "added votes for {}".format(", ".join(songs[int(i)] for i in votes))
+        case "vote-by-song":
+            songs = list(data_store.get_songs())
+            index, votes = arguments.split()
+            song = songs[int(index)]
+            data_store.add_votes_to_song(song, int(votes))
+            return "song {} now has {} votes".format(song, data_store.get_votes(song))
+        case "tally":
+            return "\n".join(player + " - " + str(score) for player, score in song_bank.make_tally_board())
+        case "detail":
+            return str(song_bank.get_detailed_breakdown(arguments))
+        case "youtube":
+            songs = list(data_store.get_songs())
+            song_title = songs[int(arguments)]
+            return get_song_youtube_link(song_title)
+        case _:
+            return "valid commands are stop, reset, songs, players, votes, list, merge, vote, vote-by-song, tally, detail, and youtube"
 
-    def run_command(command, arguments):
-        match command:
-            case "reset":
-                data_store.reset_tables()
-                print("all data wiped")
-            case "list":
-                player, songs = arguments.split(" ", 1)
-                song_bank.add_player(player, [format_title(title) for title in songs.split(",")])
-                print("added songs for player", player)
-            case "vote":
-                votes = set(arguments.split())
-                songs = list(data_store.get_songs())
-                if len(votes) > len(songs):
-                    raise IndexError()
-                song_bank.add_votes([songs[int(i)] for i in votes])
-                print("added votes for {}".format(", ".join(songs[int(i)] for i in votes)))
-            case "vote-by-song":
-                songs = list(data_store.get_songs())
-                index, votes = arguments.split()
-                song = songs[int(index)]
-                data_store.add_votes_to_song(song, int(votes))
-                print("song {} now has {} votes".format(song, data_store.get_votes(song)))
-            case "merge":
-                index1, index2 = arguments.split()
-                songs = list(data_store.get_songs())
-                song_bank.merge_songs(songs[int(index1)], songs[int(index2)])
-                print("merged song {} into song {}".format(songs[int(index1)], songs[int(index2)]))
-            case "tally":
-                for player, score in song_bank.make_tally_board():
-                    print(player, score)
-            case "detail":
-                print(song_bank.get_detailed_breakdown(arguments))
-            case "youtube":
-                songs = list(data_store.get_songs())
-                song_title = songs[int(arguments)]
-                print(get_song_youtube_link(song_title))
-            case _:
-                return
-
+def run_cli():
     command, arguments = find_arguments(input())
     while command != "stop":
-        run_command(command, arguments)
-        players = data_store.get_all_players()
-        if players:
-            print(", ".join(players))
-        songs = list(data_store.get_songs())
-        if songs:
-            print("songs:")
-            for i in range(len(songs)):
-                print(i, "-", songs[i], "(" + get_song_youtube_link(songs[i]) + ")")
+        response = run_command(command, arguments)
+        print(response)
         command, arguments = find_arguments(input())
 
 if __name__ == "__main__":
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,3 +10,51 @@ def test_find_arguments():
     assert cli.find_arguments("merge 1 1'") == ("merge", "1 1'")
     assert cli.find_arguments("show-score") == ("show-score", None)
     assert cli.find_arguments("add-list person song 1, song 2") == ("add-list", "person song 1, song 2")
+
+def test_youtube():
+    song_title = "Barrett's Privateers - Stan Rogers"
+    assert cli.get_song_youtube_link(song_title) == "https://www.youtube.com/results?search_query=Barrett%27s+Privateers+-+Stan+Rogers"
+
+def test_run_command():
+    cli.run_command("reset", None)
+
+    assert cli.run_command("songs", None) == ""
+    assert cli.run_command("players", None) == ""
+    assert cli.run_command("votes", None) == ""
+
+    cli.run_command("list", "1 A-A, B-B")
+
+    assert cli.run_command("songs", None) == "0 - A - A (https://www.youtube.com/results?search_query=A+-+A)\n1 - B - B (https://www.youtube.com/results?search_query=B+-+B)"
+    assert cli.run_command("players", None) == "1"
+    assert cli.run_command("votes", None) == "A - A: 0\nB - B: 0"
+
+    assert cli.run_command("youtube", "1") == "https://www.youtube.com/results?search_query=B+-+B"
+
+    cli.run_command("list", "2 A-A', C-C")
+
+    assert cli.run_command("players", None) == "1, 2"
+    assert cli.run_command("votes", None) == "A - A: 0\nA - A': 0\nB - B: 0\nC - C: 0"
+
+    cli.run_command("merge", "0 1")
+
+    assert cli.run_command("players", None) == "1, 2"
+    assert cli.run_command("votes", None) == "A - A: 0\nB - B: 0\nC - C: 0"
+
+    cli.run_command("vote", "0 1")
+
+    assert cli.run_command("votes", None) == "A - A: 1\nB - B: 1\nC - C: 0"
+
+    cli.run_command("vote-by-song", "2 3")
+
+    assert cli.run_command("votes", None) == "A - A: 1\nB - B: 1\nC - C: 3"
+
+    assert cli.run_command("tally", None) == "2 - 2.0\n1 - 0.0"
+
+    assert cli.run_command("detail", "1") == "total: 0.0, songs: {'A - A': 0.0, 'B - B': 0.0}"
+    assert cli.run_command("detail", "2") == "total: 2.0, songs: {'A - A': 0.0, 'C - C': 2.0}"
+
+    cli.run_command("reset", None)
+
+    assert cli.run_command("songs", None) == ""
+    assert cli.run_command("players", None) == ""
+    assert cli.run_command("votes", None) == ""
