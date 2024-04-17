aeb3210b174108fc05377c03a7fe853bcf45c277
Matthew Gurevitch <matthewgurevitch@gmail.com>
2024-04-17 16:07:14 -0400

    adding in help to cli

--- cli.py
+++ cli.py
@@ -5,6 +5,21 @@ from src.Scorer import Scorer
 from enum import Enum
 
 Phase = Enum('Phase', ['ADD_LIST', 'VOTE', 'SCORE'])
+commands = {
+    "stop": {"phase": None, "arguments": None, "description": "closes the cli"},
+    "help": {"phase": None, "arguments": "command_name", "description": "gives the description of given command"}, 
+    "reset": {"phase": None, "arguments": None, "description": "resets the databases to be null"},
+    "songs": {"phase": None, "arguments": None, "description": "lists all songs with index and youtube link"},
+    "players": {"phase": None, "arguments": None, "description": "lists all players"},
+    "votes": {"phase": None, "arguments": None, "description": "lists all songs with votes"},
+    "youtube": {"phase": None, "arguments": "song_index", "description": "gets a link to a youtube search for the song of given index"},
+    "list": {"phase": Phase.ADD_LIST, "arguments": "player <comma separated list of song names for the player>", "description": "adds the list for a given player"},
+    "merge": {"phase": Phase.ADD_LIST, "arguments": "index_1 index_2", "description": "merges the song from index_2 into index_1"},
+    "vote": {"phase": Phase.VOTE, "arguments": "space separated list of song indices", "description": "casts a vote for songs of the listed indices" },
+    "vote-by-song": {"phase": Phase.VOTE, "arguments": "song_index number_of_votes", "description": "casts multiple votes for the song of the given index"},
+    "tally": {"phase": Phase.SCORE, "arguments": None, "description": "gives a tally of players and final votes, ordered from most to least votes"},
+    "detail": {"phase": Phase.SCORE, "arguments": "player", "description": "gives a detailed list of which songs gave the given player points"}
+}
 
 def format_title(raw_title: str) -> str:
     song_name, artist = raw_title.split("-", 1)
@@ -28,7 +43,8 @@ class CLI:
 
     def check_phase(self, phase: Phase):
         if self.phase != phase:
-            print("changing phases from {} to {}".format(self.phase, phase))
+            if self.phase:
+                print("changing phases from {} to {}".format(self.phase.name, phase.name))
             self.phase = phase
             match phase:
                 case Phase.ADD_LIST:
@@ -45,7 +61,16 @@ class CLI:
                     self.scorer = Scorer(self.is_test)
 
     def run_command(self, command: str, arguments: str | None) -> str:
+        if command in commands and commands[command]["phase"]:
+            self.check_phase(commands[command]["phase"])
         match command:
+            case "help":
+                if not arguments:
+                    return "valid commands are " + ", ".join(commands.keys())
+                command_help = commands[arguments]
+                phase = command_help["phase"].name if command_help["phase"] else "All Phases"
+                arguments = command_help["arguments"] if command_help["arguments"] else ""
+                return "Phase: {}\nArguments: {}\nDescription: {}".format(phase, arguments, command_help["description"])
             case "reset":
                 self.data_store.reset_tables()
                 return "all data wiped"
@@ -58,19 +83,20 @@ class CLI:
             case "votes":
                 songs = list(self.data_store.get_songs())
                 return "\n".join(song + ": " + str(self.data_store.get_votes(song)) for song in songs)
+            case "youtube":
+                songs = list(self.data_store.get_songs())
+                song_title = songs[int(arguments)]
+                return get_song_youtube_link(song_title)
             case "list":
-                self.check_phase(Phase.ADD_LIST)
                 player, songs = arguments.split(" ", 1)
                 self.list_adder.add_player(player, [format_title(title) for title in songs.split(",")])
                 return "added songs for player " + player
             case "merge":
-                self.check_phase(Phase.ADD_LIST)
                 index1, index2 = arguments.split()
                 songs = list(self.data_store.get_songs())
                 self.list_adder.merge_songs(songs[int(index1)], songs[int(index2)])
                 return "merged song {} into song {}".format(songs[int(index1)], songs[int(index2)])
             case "vote":
-                self.check_phase(Phase.VOTE)
                 votes = set(arguments.split())
                 songs = list(self.data_store.get_songs())
                 if len(votes) > len(songs):
@@ -78,24 +104,17 @@ class CLI:
                 self.voter.add_votes([songs[int(i)] for i in votes])
                 return "added votes for {}".format(", ".join(songs[int(i)] for i in votes))
             case "vote-by-song":
-                self.check_phase(Phase.VOTE)
                 songs = list(self.data_store.get_songs())
                 index, votes = arguments.split()
                 song = songs[int(index)]
                 self.voter.add_votes_to_song(song, int(votes))
                 return "song {} now has {} votes".format(song, self.data_store.get_votes(song))
             case "tally":
-                self.check_phase(Phase.SCORE)
                 return "\n".join(player + " - " + str(score) for player, score in self.scorer.make_tally_board())
             case "detail":
-                self.check_phase(Phase.SCORE)
                 return str(self.scorer.get_detailed_breakdown(arguments))
-            case "youtube":
-                songs = list(self.data_store.get_songs())
-                song_title = songs[int(arguments)]
-                return get_song_youtube_link(song_title)
             case _:
-                return "valid commands are stop, reset, songs, players, votes, list, merge, vote, vote-by-song, tally, detail, and youtube"
+                return "valid commands are " + ", ".join(commands.keys())
 
     def run_cli(self):
         print("players: " + self.run_command("players", None))
