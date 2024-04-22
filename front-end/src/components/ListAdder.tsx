faaf26164a8c759998f49f5caf80e4ed03a135d3
Matthew Gurevitch <matthewgurevitch@gmail.com>
2024-04-22 15:06:55 -0400

    adding space on frontend for artist

--- front-end/src/components/ListAdder.tsx
+++ front-end/src/components/ListAdder.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import API from "./API";
 
+type Song = {
+    song: string;
+    artist: string;
+}
+
 const ListAdder = () => {
     const [players, setPlayers] = useState<string[]>([]);
-    const [formValues, setFormValues] = useState<{name: string, songs: string[]}>({name: "", songs: [""]})
+    const [formValues, setFormValues] = useState<{name: string, songs: Song[]}>({name: "", songs: [{song: "", artist: ""}]})
 
     useEffect(() => {
         API.getAllPlayers().then(response => setPlayers(response.players));
@@ -15,7 +20,7 @@ const ListAdder = () => {
             console.log("Already submitted");
             return;
         }
-        await API.addPlayerList(formValues.name, formValues.songs.filter(x => x));
+        await API.addPlayerList(formValues.name, formValues.songs.filter(x => x.song).map(x => `${x.song} - ${x.artist || "Unknown"}`));
         let newPlayers = await API.getAllPlayers();
         setPlayers(newPlayers.players);
     };
@@ -25,15 +30,19 @@ const ListAdder = () => {
         setFormValues({ ...formValues, name: event.target.value });
     };
 
-    const handleSongsChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleSongsChange = (index: number, isArtist: boolean) => (event: React.ChangeEvent<HTMLInputElement>) => {
         event.preventDefault();
         let songs = formValues.songs;
-        songs[index] = event.target.value;
-        if (!event.target.value) {
-            songs = songs.filter(x => x);
-            songs.push("");
+        if(isArtist) {
+            songs[index].artist = event.target.value;
+        } else {
+            songs[index].song = event.target.value;
+        }
+        if (!songs[index].song) {
+            songs = songs.filter(x => x.song);
+            songs.push({song: "", artist: ""});
         } else if (index+1 === songs.length) {
-            songs.push("");
+            songs.push({song: "", artist: ""});
         }
         setFormValues({ ...formValues, songs });
     };
@@ -44,9 +53,13 @@ const ListAdder = () => {
         <form onSubmit={handleSubmit}>
             <label>Name: <input type="text" key="name" onChange={handleNameChange} /></label>
             <br/>
-            <label>Songs:</label>
+            <label>Songs - Artists:</label>
             {
-                formValues.songs.map((value, index) => <div><input type="text" key={`song${index}`} onChange={handleSongsChange(index)} value={value} /></div>)
+                formValues.songs.map((value, index) => <div>
+                    <input type="text" key={`song${index}`} onChange={handleSongsChange(index, false)} value={value.song} />
+                    -
+                    <input type="text" key={`artist${index}`} onChange={handleSongsChange(index, true)} value={value.artist} />
+                </div>)
             }
             <button type="submit">Submit</button>
         </form>
